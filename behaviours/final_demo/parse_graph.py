ef01bf42032bb8d524cc4c4cc3ffbd0f79f364ab
= <dannyrogaar@hotmail.com>
2019-07-08 12:48:54 +0200

    Updated orientation codes and status

--- behaviours/final_demo/parse_graph.py
+++ behaviours/final_demo/parse_graph.py
@@ -3,118 +3,119 @@ import numpy as np
 
 import sys
 sys.path.append("/home/student/sudo/ros/catkin_ws/src/navigation_test_planner/scripts")
-from dijkstra_planning import Dijkstra
+# from dijkstra_planning import Dijkstra
 
 import rospy
-from alice_msgs.srv import MakePlan, MakePlanResponse
+# from alice_msgs.srv import MakePlan, MakePlanResponse
 import heapq
 import math
 
 class Dijkstra2(object):
-    def __init__(self, graph):
-        self.graph = graph
-
-    def euclidean_distance(self, start, end):
-            '''inputs: start and end (node string names)
-            output: euclidean distance between the two points'''
-
-            start = self.graph.waypoint_of(start)['position']
-            end = self.graph.waypoint_of(end)['position']
-
-            # calculate the euclidean distance
-            x_diff = end['x'] - start['x']
-            y_diff = end['y'] - start['y']
-            z_diff = end['z'] - start['z']
-            return math.sqrt(x_diff**2 + y_diff**2 + z_diff**2)
-
-    def find_neighbors(self, index, width, map_size, costmap_list):
-            nodes = self.graph.nodes()
-
-            # Grab neighbouring nodes
-            current_wp = self.graph.index2waypoint[index]
-            neighbs = self.graph.connections[current_wp]
-
-            # Append distances
-            neighbs = [[self.graph.waypoint2index[n], self.euclidean_distance(current_wp, n)] for n in neighbs]
-            filtered = [n for n in neighbs if costmap_list[n[0]] == 0]
-
-            # print('filtered neighbors: ', [self.graph.index2waypoint[n[0]] for n in filtered])
-            # print('neighbors distances: ', [n[1] for n in filtered])
-            return filtered
-
-    def make_plan(self, req):
-        ## this is the data you get from the request
-        ## the costmap, a single array version of an image
-        costmap = req.costmap_ros
-        width = req.width
-        height = req.height
-        map_size = height * width
-        start_index = req.start
-        goal_index = req.goal
-
-        costmap_list = list(costmap)
-        place_holder = [x for x in costmap_list if x != 0]
-        neighbors = []
-        distance = [float("inf")] * map_size
-        distance[start_index] = 0
-        previous = [float("inf")] * map_size
-        pq = []
-
-        entry = [0, start_index]
-        heapq.heappush(pq, entry)
-
-        while pq:
-            current_distance, current_vertex = heapq.heappop(pq)
-            # print('current_vertex and current_distance: ', self.graph.index2waypoint[current_vertex], current_distance)
-            if current_vertex == goal_index:
-                break
-
-            neighbors = self.find_neighbors(current_vertex, width, map_size, costmap_list)
-
-            # check if node can be visited
-            # it cannot be visited if costmap value is not 0
-            costmap_list[current_vertex] = 1
-
-            for neighbor in neighbors:
-                neighbor_distance = neighbor[1]
-                neighbor_index = neighbor[0]
-
-                # distance = current_distance + neighbor_distance
-
-                if (distance[neighbor_index] > (distance[current_vertex] + neighbor_distance)):
-                    # push the node onto the heap
-                    distance[neighbor_index] = distance[current_vertex] + neighbor_distance
-                    previous[neighbor_index] = current_vertex
-                    entry = [distance[neighbor_index], neighbor_index]
-                    heapq.heappush(pq, entry)
-
-        # creation of path array
-        path = []
-        current_node = goal_index
-        path.append(current_node)
-
-        while True:
-            parent = previous[current_node]
-            if parent == start_index:
-                break
-            elif parent == float("inf"):
-                break
-            path.append(parent)
-            current_node = parent
-
-        # path.append(previous[path[-1]])
-
-        # print("path.append(previous[path[-1]])", self.graph.index2waypoint[previous[path[-1]]])
-        # print("prepending " + str(self.graph.index2waypoint[previous[path[-1]]] + " to path. (" + str(previous[path[-1]]) + ")"))
-        path = path[::-1]
-        # print(path)
-
-        # print("previous of first", self.graph.index2waypoint[previous[path[0]]])
-
-        #make a response object
-        resp = MakePlanResponse()
-        resp.plan = path
-        return resp
+    pass
+#     def __init__(self, graph):
+#         self.graph = graph
+#
+#     def euclidean_distance(self, start, end):
+#             '''inputs: start and end (node string names)
+#             output: euclidean distance between the two points'''
+#
+#             start = self.graph.waypoint_of(start)['position']
+#             end = self.graph.waypoint_of(end)['position']
+#
+#             # calculate the euclidean distance
+#             x_diff = end['x'] - start['x']
+#             y_diff = end['y'] - start['y']
+#             z_diff = end['z'] - start['z']
+#             return math.sqrt(x_diff**2 + y_diff**2 + z_diff**2)
+#
+#     def find_neighbors(self, index, width, map_size, costmap_list):
+#             nodes = self.graph.nodes()
+#
+#             # Grab neighbouring nodes
+#             current_wp = self.graph.index2waypoint[index]
+#             neighbs = self.graph.connections[current_wp]
+#
+#             # Append distances
+#             neighbs = [[self.graph.waypoint2index[n], self.euclidean_distance(current_wp, n)] for n in neighbs]
+#             filtered = [n for n in neighbs if costmap_list[n[0]] == 0]
+#
+#             # print('filtered neighbors: ', [self.graph.index2waypoint[n[0]] for n in filtered])
+#             # print('neighbors distances: ', [n[1] for n in filtered])
+#             return filtered
+#
+#     def make_plan(self, req):
+#         ## this is the data you get from the request
+#         ## the costmap, a single array version of an image
+#         costmap = req.costmap_ros
+#         width = req.width
+#         height = req.height
+#         map_size = height * width
+#         start_index = req.start
+#         goal_index = req.goal
+#
+#         costmap_list = list(costmap)
+#         place_holder = [x for x in costmap_list if x != 0]
+#         neighbors = []
+#         distance = [float("inf")] * map_size
+#         distance[start_index] = 0
+#         previous = [float("inf")] * map_size
+#         pq = []
+#
+#         entry = [0, start_index]
+#         heapq.heappush(pq, entry)
+#
+#         while pq:
+#             current_distance, current_vertex = heapq.heappop(pq)
+#             # print('current_vertex and current_distance: ', self.graph.index2waypoint[current_vertex], current_distance)
+#             if current_vertex == goal_index:
+#                 break
+#
+#             neighbors = self.find_neighbors(current_vertex, width, map_size, costmap_list)
+#
+#             # check if node can be visited
+#             # it cannot be visited if costmap value is not 0
+#             costmap_list[current_vertex] = 1
+#
+#             for neighbor in neighbors:
+#                 neighbor_distance = neighbor[1]
+#                 neighbor_index = neighbor[0]
+#
+#                 # distance = current_distance + neighbor_distance
+#
+#                 if (distance[neighbor_index] > (distance[current_vertex] + neighbor_distance)):
+#                     # push the node onto the heap
+#                     distance[neighbor_index] = distance[current_vertex] + neighbor_distance
+#                     previous[neighbor_index] = current_vertex
+#                     entry = [distance[neighbor_index], neighbor_index]
+#                     heapq.heappush(pq, entry)
+#
+#         # creation of path array
+#         path = []
+#         current_node = goal_index
+#         path.append(current_node)
+#
+#         while True:
+#             parent = previous[current_node]
+#             if parent == start_index:
+#                 break
+#             elif parent == float("inf"):
+#                 break
+#             path.append(parent)
+#             current_node = parent
+#
+#         # path.append(previous[path[-1]])
+#
+#         # print("path.append(previous[path[-1]])", self.graph.index2waypoint[previous[path[-1]]])
+#         # print("prepending " + str(self.graph.index2waypoint[previous[path[-1]]] + " to path. (" + str(previous[path[-1]]) + ")"))
+#         path = path[::-1]
+#         # print(path)
+#
+#         # print("previous of first", self.graph.index2waypoint[previous[path[0]]])
+#
+#         #make a response object
+#         resp = MakePlanResponse()
+#         resp.plan = path
+#         return resp
 
 
 class Graph():
@@ -136,8 +137,24 @@ class Graph():
         self.waypoint2index = {n : idx for idx, n in enumerate(self.nodes())}
         self.index2waypoint = {v : k for k,v in self.waypoint2index.items()}
         self.waypoints = waypoints
+        self.wp_aligned = []
 
-        self.planner = Dijkstra2(self)
+
+
+        # initial quaternions for co-ordinate system(for simulation)
+        # needs to be caliberated for irl
+        # AXIS SYSTEM: VIEWING FROM DEMO WORKTABLE TOWARD ROBOT
+        # LEFT = POSITIVE X AXIS, FRONT = POSTIVE Y AXIS IN SIMULATION
+        # LEFT = POSITIVE X AXIS, FRONT = NEGATIVE Y AXIS IRL
+        # CHECK THESE TOO TO BE SURE :P
+        self.coordinate_system()
+
+        self.positive_x_axis = {'x': 0.000, 'y': 0.000, 'z': -0.012, 'w': 1.000}
+        self.positive_y_axis = {'x': 0.000, 'y': 0.000, 'z': 0.708, 'w': 0.706}
+        self.negative_x_axis = {'x': 0.000, 'y': 0.000, 'z': 1.000, 'w': -0.005}
+        self.negative_y_axis = {'x': 0.000, 'y': 0.000, 'z': -0.696, 'w': 0.719}
+
+        # self.planner = Dijkstra2(self)
         # self.override_planner()
         # print(d_planner)
 
@@ -198,6 +215,9 @@ class Graph():
     def waypoint_of(self, node):
         return [wp for wp in self.waypoints if wp['name'].lower() == node][0]
 
+    def aligned_wp_of(self, node):
+        return [wp for wp in self.wp_aligned if wp['name'].lower() == node][0]
+
     def path(self, start, end):
         class struct(object):
             pass
@@ -214,6 +234,83 @@ class Graph():
         path = [self.index2waypoint[index] for index in path]
         return path
 
+    def coordinate_system(self):
+        '''assign a custom axis system to waypoints for calculations'''
+        # [print(wp['position']) for wp in self.waypoints]
+
+        posbyname = {wp['name'] : np.array([wp['position']['x'], wp['position']['y'], wp['position']['z']]) for wp in self.waypoints}
+        wpbypos = {tuple(pos) : self.waypoint_of(name) for name, pos in posbyname.items()}
+
+        scalar_proj = lambda vec, axis : np.dot(vec, axis) / np.sqrt(np.dot(axis, axis))
+
+        # avg of horizontal diffs becomes horizontal axis. y axis orthogonal
+        # print(posbyname['table1_right'] - posbyname['table1_left'])
+        mat = np.stack([posbyname['table1_right'] - posbyname['table1_left'],
+                                        posbyname['table1_right'] - posbyname['table2'],
+                                        posbyname['table1_left'] - posbyname['table2'],
+                                        posbyname['mid_right'] - posbyname['mid_center'],
+                                        posbyname['mid_right'] - posbyname['mid_left'],
+                                        posbyname['mid_center'] - posbyname['mid_left']])
+        self.rightward_axis = np.mean(mat, axis=0)
+        print('rightward_axis', self.rightward_axis)
+        self.frontward_axis = np.array([-self.rightward_axis[1], self.rightward_axis[0], self.rightward_axis[2]])   #ortho
+        self.frontward_axis = self.frontward_axis * scalar_proj(posbyname['table2'] - posbyname['mid_left'], self.frontward_axis)             #project correct direction onto axes
+
+        self.wp_aligned = []
+        for pos in posbyname.values():
+            wp = wpbypos[tuple(pos)]
+            wp['position']['x'] = scalar_proj(pos, self.rightward_axis)
+            wp['position']['y'] = scalar_proj(pos, self.frontward_axis)
+            wp['position']['z'] = 0.0
+            self.wp_aligned.append(wp)
+
+
+    def assign_orientation(self, wp_start, wp_end):
+        '''When moving from A to B, calculate a decent orientation for the robots waypoint'''
+
+        # unit vector for each axis
+        # instead of unit vectors, store in the quaternions for each axis
+        # correspondence 0 --> x, 1 --> y, 2 --> z
+        positive_axis_orientation = {0: self.positive_x_axis, 1: self.positive_y_axis}
+        negative_axis_orientation = {0: self.negative_x_axis, 1: self.negative_y_axis}
+        # positive_axis_orientation = {0: self.rightward_axis, 1: self.frontward_axis}
+        # negative_axis_orientation = {0: -self.rightward_axis, 1: -self.frontward_axis}
+
+        wp_start = self.aligned_wp_of(wp_start['name'])
+        wp_end = self.aligned_wp_of(wp_end['name'])
+
+        x_diff = wp_end['position']['x'] - wp_start['position']['x']
+        y_diff = wp_end['position']['y'] - wp_start['position']['y']
+
+        self.grasp_locations = ['table1_left', 'table1_right', 'table2']
+        if wp_end['name'].lower() not in self.grasp_locations:
+            # absolute difference in each axis
+            abs_differences = [abs(x_diff), abs(y_diff)]
+
+            # extract the axis with the maximum difference
+            # max_index = abs_differences.index(max(abs_differences))
+            max_index = np.argmax(abs_differences)
+            max_index = {0 : 'x', 1 : 'y'}[max_index]
+
+            # extracts the sign for the orientation vector
+            polarity = (wp_end['position'][max_index] - wp_start['position'][max_index])
+
+            if polarity > 0:
+                max_index = {'x' : 0, 'y' : 1}[max_index]
+                orientation = positive_axis_orientation[max_index]
+            else:
+                max_index = {'x' : 0, 'y' : 1}[max_index]
+                orientation = negative_axis_orientation[max_index]
+
+
+        # special case in which end state is in any of the grasp locations
+        elif wp_end['name'].lower() in self.grasp_locations:
+            orient = {'table1_left' : positive_axis_orientation[0], 'table1_right' : negative_axis_orientation[0], 'table2' : negative_axis_orientation[0]}
+            orientation = orient[wp_end['name'].lower()]
+
+        # returns a quaternion
+        return orientation
+
 if __name__ == "__main__":
     import yaml
 
@@ -221,10 +318,10 @@ if __name__ == "__main__":
     graph_nodes = None
     with open(dirname + 'simulation_waypoints.yaml', 'r') as f:
         graph_nodes = yaml.load(f)
- 
+
     graph = Graph(graph_nodes)
     # print(graph.connections)
-    # print(graph.nodes())    
+    # print(graph.nodes())
 
     print("'start', 'table1_left'", graph.path('start', 'table1_left'))
     print("'start', 'table2'", graph.path('start', 'table2'))
@@ -239,4 +336,3 @@ if __name__ == "__main__":
     print("'table1_left', 'table2'", graph.path('table1_left', 'table2'))
 
     # print(graph.connections['table2'])
-
--- status.py
+++ status.py
@@ -23,24 +23,29 @@ class TestNavigation(unittest.TestCase):
             self.vis.node(node)
 
     def test_dynamic_rotations(self):
-        from behaviours.final_demo.orientation import Definitions, assign_orientation
+        # from behaviours.final_demo.orientation import Definitions, assign_orientation
 
         # map start to end node with their outcome vectors
-        outcomes = {('mid_left', 'table1_left')     : Definitions.positive_x_axis,#
-                    ('mid_left', 'table2')          : Definitions.negative_x_axis,#
-                    ('mid_left', 'mid_right')       : Definitions.positive_x_axis,#
-                    ('mid_left', 'mid_center')      : Definitions.positive_x_axis,#
-                    ('table2', 'mid_left')          : Definitions.negative_y_axis,##
-                    ('table1_left', 'mid_left')     : Definitions.negative_y_axis,##
-                    ('mid_right', 'mid_left')       : Definitions.negative_x_axis,#
-                    ('mid_right', 'table1_right')   : Definitions.positive_y_axis,#
-                    ('mid_right', 'mid_center')     : Definitions.negative_x_axis,#
-                    ('table1_right', 'mid_right')   : Definitions.negative_y_axis,#
-                    ('mid_center', 'mid_left')      : Definitions.negative_x_axis,#
-                    ('mid_center', 'mid_right')     : Definitions.positive_x_axis,#
-                    ('mid_center', 'drop-off')      : Definitions.negative_x_axis,#
-                    ('drop-off', 'mid_center')      : Definitions.positive_x_axis,#
+        outcomes = {('mid_left', 'table1_left')     : [self.graph.positive_x_axis],#
+                    ('mid_left', 'table2')          : [self.graph.negative_x_axis],#
+                    ('mid_left', 'mid_right')       : [self.graph.positive_x_axis],#
+                    ('mid_left', 'mid_center')      : [self.graph.positive_x_axis],#
+                    ('table2', 'mid_left')          : [self.graph.negative_y_axis, self.graph.positive_x_axis],##
+                    ('table1_left', 'mid_left')     : [self.graph.negative_y_axis],##
+                    ('mid_right', 'mid_left')       : [self.graph.negative_x_axis],#
+                    ('mid_right', 'table1_right')   : [self.graph.negative_x_axis],#
+                    ('mid_right', 'mid_center')     : [self.graph.negative_x_axis],#
+                    ('table1_right', 'mid_right')   : [self.graph.negative_y_axis],#
+                    ('mid_center', 'mid_left')      : [self.graph.negative_x_axis],#
+                    ('mid_center', 'mid_right')     : [self.graph.positive_x_axis],#
+                    ('mid_center', 'drop-off')      : [self.graph.negative_y_axis],#
+                    ('drop-off', 'mid_center')      : [self.graph.positive_y_axis],#
         }
+
+        def foo(node_dict):
+            for prop in ['positive_x_axis', 'positive_y_axis', 'negative_x_axis', 'negative_y_axis']:
+                if self.graph.__dict__[prop] == node_dict:
+                    return prop
 
         succeed = True
         for start_node in self.graph.connections.keys():
@@ -48,15 +53,16 @@ class TestNavigation(unittest.TestCase):
                 if start_node == 'start':
                     continue
 
-                start = self.graph.waypoint_of(start_node)['position']
-                end = self.graph.waypoint_of(end_node)['position']
-                output = assign_orientation(start, end, self.graph.waypoint_of(end_node)['name'])
+                start = self.graph.waypoint_of(start_node)#['position']
+                end = self.graph.waypoint_of(end_node)#['position']
+                output = self.graph.assign_orientation(start, end)
+                # output = assign_orientation(start, end, self.graph.waypoint_of(end_node)['name'])
 
-                expected_outcome = output == outcomes[(start_node, end_node)]
+                expected_outcome = output in outcomes[(start_node, end_node)]
                 if not expected_outcome:
                     succeed = False
                     self.vis.edge(start_node, end_node, color='red')
-                    print('FAILED: start', start_node, 'end', end_node, 'output', output, 'expected_output', outcomes[(start_node, end_node)])
+                    print('FAILED: start', start_node, 'end', end_node, 'output', foo(output), 'expected_output', [foo(out) for out in outcomes[(start_node, end_node)]])
                 else:
                     self.vis.edge(start_node, end_node, color='black')
 
